1e735592aa35b497e99bb5b44d3470949f809825
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-12-06 14:57:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return '@JishuDeveloper'
 
-
 if __name__ == "__main__":
-    app.run()
+    PORT = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=PORT)
